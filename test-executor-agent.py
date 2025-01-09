49997a9982d11d0dd3a3f09a2af6fb2f94f892c1
nandkumar ghatge <nandkumar.ghatge@smartbox.com>
2025-01-09 15:26:14 +0000

    added execution report

--- test-executor-agent.py
+++ test-executor-agent.py
@@ -9,7 +9,7 @@ Execute BDD style test cases from feature file
 """
 test_case_agent.run(
    """
-   can you execute BDD style test case using pytest-bdd using task "read_file" return test execution report in json format using task "report"
+   can you Execute BDD test case using pytest-bdd using task "read_file" return test execution report in json format using task "report"
    additional_args={"filename":'api_test.feature'}
    """
 )
