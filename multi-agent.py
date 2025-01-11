4ca1bc9e482fdc60a1929f00f3e34803dd82d640
nandkumar ghatge <nandkumar.ghatge@smartbox.com>
2025-01-11 13:40:16 +0000

    added multi agent example

---
+++ multi-agent.py
@@ -0,0 +1,34 @@
+from smolagents import (
+    CodeAgent,
+    ToolCallingAgent,
+    HfApiModel,
+    ManagedAgent,
+    DuckDuckGoSearchTool,
+    LiteLLMModel,
+)
+
+from tools.tools import visit_webpage
+
+
+web_agent = ToolCallingAgent(
+    tools=[DuckDuckGoSearchTool(), visit_webpage],
+    model=HfApiModel(),
+    max_steps=10,
+)
+
+managed_web_agent = ManagedAgent(
+    agent=web_agent,
+    name="search",
+    description="Runs web searches for you. Give it your query as an argument.",
+)
+
+manager_agent = CodeAgent(
+    tools=[],
+    model=HfApiModel(),
+    managed_agents=[managed_web_agent],
+    additional_authorized_imports=["time", "numpy", "pandas"],
+)
+
+answer = manager_agent.run("If LLM training continues to scale up at the current rhythm until 2030, what would be the electric power in GW required to power the biggest training runs by 2030? What would that correspond to, compared to some countries? Please provide a source for any numbers used.")
+
+print(answer)
